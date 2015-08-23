2775a2bfc8916eb1e006b8a08f6a86adcdfeae65
Rosie Hoyem <rosiehoyem@gmail.com>
2015-08-23 13:14:10 -0500

    updated dropbox initializer

--- config/initializers/drop_box.rb
+++ config/initializers/drop_box.rb
@@ -5,4 +5,4 @@ require 'dropbox_sdk'
 DROPBOX_APP_KEY = Rails.configuration.dropbox_app_key
 DROPBOX_APP_SECRET = Rails.configuration.dropbox_app_secret
 
-flow = DropboxOAuth2FlowNoRedirect.new(DROPBOX_APP_KEY, DROPBOX_APP_SECRET)
+# flow = DropboxOAuth2FlowNoRedirect.new(DROPBOX_APP_KEY, DROPBOX_APP_SECRET)
